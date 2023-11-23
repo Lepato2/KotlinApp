8460af7e8e8040ffaa4e7e6092acb00fbf0f0415
Lepato2 <cullampeluisenrique@gmail.com>
2023-11-23 12:22:37 -0500

    init asdsdfsd

---
+++ app/src/main/java/com/optic/kotlinudemydelivery/activities/client/products/detail/ClientProductsDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.optic.kotlinudemydelivery.activities.client.products.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.optic.kotlinudemydelivery.R
+
+class ClientProductsDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_client_products_detail)
+    }
+}
